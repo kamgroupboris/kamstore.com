e03f8c7398c16954ff3753b7ac936857f2da66e8
kamgroupboris <kamgroupboris@yandex.ru>
2016-05-19 20:00:09 +0300

    GridView 'content'

--- views/items/update.php
+++ views/items/update.php
@@ -1,6 +1,8 @@
 <?php
 
 use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\grid\DataColumn;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Products */
@@ -14,8 +16,36 @@ $this->params['breadcrumbs'][] = 'Update';
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
+    <?= $this->render('_product', [
         'model' => $model,
     ]) ?>
+
+</div>
+<div class="options-index">
+
+    <p>
+        <?= Html::a('Create Options', ['/options/create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            'id',
+            [
+                'attribute' => 'name',
+                'format' => 'text',
+                'label' => 'Имя',
+            ],
+            [
+                'attribute' => 'value',
+                'format' => 'html',
+                'label' => 'Значение',
+                'content'=> function($data){return Html::input('text', 'username', $data['value'],['class' => 'form-control']);},
+
+            ],
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
 
 </div>
